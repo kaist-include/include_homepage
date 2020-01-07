9752523a43957e9e303bb90091f6d769ba719a6a
seoyun <seoyun.son@kaist.ac.kr>
2020-01-07 23:55:48 +0900

    ADD: Default Home Template

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo_simple.png';
 import './App.css';
 
 const App: React.FC = () => {
@@ -8,7 +8,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <h1>INCLUDE KAIST</h1>
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          About
         </a>
       </header>
     </div>
